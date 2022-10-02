e3aae301e8555b3c445c2fe8a8d6e9f5ee531242
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-02 13:14:36 -0700

    push project1_expressions_alert folder

---
+++ Basic Javascript Projects/project1_expressions_alert/js/main.js
@@ -0,0 +1,21 @@
+window.alert("Hello, World!");
+document.write("Hello, Universe!");
+
+var A = "This is variable A";
+document.write(A); //assigned variable and produced onto document
+
+var B = "concatenated a string";
+window.alert(B); //assigned variable and produced into window alert box
+
+document.write("Then my friend yelled,\"And that's the way the cookie crumbles.\""); //escaping character
+
+document.write ("bing bang boom"
++ " this is a new line of string"); //concatenated a string
+
+var C = "this is variable C" + " and here's another";
+document.write(C); //assigned variable and string concatenation
+
+var Desserts = "flavor", cake = "chocolate", tart = "apple", donut = "strawberry";
+document.write (donut); //assigned multiple variables to values
+
+document.write(4+4); //expression
