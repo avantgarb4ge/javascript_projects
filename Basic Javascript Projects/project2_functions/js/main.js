de00b88fc6375652733795e32abff9b3727ca559
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-02 23:18:58 -0700

    updated main.js

--- Basic Javascript Projects/project2_functions/js/main.js
+++ Basic Javascript Projects/project2_functions/js/main.js
@@ -1,11 +1,11 @@
-function pickDessert() {
-    var single = "vanilla cupcake";
+function pickDessert() { //naming function
+    var single = "vanilla cupcake"; //assigning value to variable
     var mult = "choclate cake";
-    document.getElementById("Button_text").innerHTML = single;
+    document.getElementById("Button_text").innerHTML = single; //returning chosen variable value
 }
 
-function savoryFood() {
+function savoryFood() { //naming function
     var salt = "pot roast";
-    salt += " is a great alternative";
-    document.getElementById("button_2").innerHTML = salt;
+    salt += " is a great alternative"; //concatenated string
+    document.getElementById("button_2").innerHTML = salt; //returning chosen variable value
 }
