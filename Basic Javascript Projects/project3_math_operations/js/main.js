867539cfb9d1a37025d15ba6780751f358f4ea1f
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-03 14:13:26 -0700

    commented on main.js

--- Basic Javascript Projects/project3_math_operations/js/main.js
+++ Basic Javascript Projects/project3_math_operations/js/main.js
@@ -1,45 +1,45 @@
 function addition() {
     var adding = 2+2;
-    document.getElementById("math").innerHTML="2 + 2 = " + adding;
+    document.getElementById("math").innerHTML="2 + 2 = " + adding; //combined sting with function output
 }
 
 function subtraction() {
     var subtracting = 10-7;
-    document.getElementById("math_2").innerHTML="10 - 7 = " + subtracting;
+    document.getElementById("math_2").innerHTML="10 - 7 = " + subtracting; //combined sting with function output
 }
 
 function multiplication() {
     var multiplying = 4*5;
-    document.getElementById("math_3").innerHTML="4 * 5 = " + multiplying;
+    document.getElementById("math_3").innerHTML="4 * 5 = " + multiplying; //combined sting with function output
 }
 
 function division() {
     var dividing = 14/2;
-    document.getElementById("math_4").innerHTML="14 / 2 = " + dividing;
+    document.getElementById("math_4").innerHTML="14 / 2 = " + dividing; //combined sting with function output
 }
 
 function mathematics() {
-    var more_math = (3+4)*6 + (4-2);
-    document.getElementById("math_5").innerHTML="(3+4)*6 + (4-2) = " + more_math;
+    var more_math = (3+4)*6 + (4-2); //multiple math operation
+    document.getElementById("math_5").innerHTML="(3+4)*6 + (4-2) = " + more_math; //combined sting with function output
 }
 
 function modulus() {
-    var remainder = 23%4;
-    document.getElementById("math_6").innerHTML="when dividing 23 by 4, the remainder is " + remainder;
+    var remainder = 23%4; // % divides and outputs remainder
+    document.getElementById("math_6").innerHTML="when dividing 23 by 4, the remainder is " + remainder; //combined sting with function output
 }
 
 function negation() {
     var x = 6;
-    document.getElementById("math_7").innerHTML="negative 6 in numerical: " + -x;
+    document.getElementById("math_7").innerHTML="negative 6 in numerical: " + -x; //adds negative sign to value
 }
 
 var y=12;
-y++;
+y++; //adds in incremental value
 document.write(y);
 
 var z=9;
-z--;
+z--; //subtracts in incremental value
 document.write(z);
 
-window.alert(Math.random());
-window.alert(Math.random() *50);
+window.alert(Math.random()); //window alert box, picks a random number
+window.alert(Math.random() *50); //window alert box, picks random number between determined value
