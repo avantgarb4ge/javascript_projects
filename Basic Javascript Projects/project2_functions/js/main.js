817df94dbd38df0eb73988c2363be2ee3414091c
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-02 23:16:26 -0700

    project2_functions

---
+++ Basic Javascript Projects/project2_functions/js/main.js
@@ -0,0 +1,11 @@
+function pickDessert() {
+    var single = "vanilla cupcake";
+    var mult = "choclate cake";
+    document.getElementById("Button_text").innerHTML = single;
+}
+
+function savoryFood() {
+    var salt = "pot roast";
+    salt += " is a great alternative";
+    document.getElementById("button_2").innerHTML = salt;
+}
