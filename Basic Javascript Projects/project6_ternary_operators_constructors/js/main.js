02d34f4b88de5b4f55699b0089b60463767b774b
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-04 15:47:45 -0700

    commented on main.js

--- Basic Javascript Projects/project6_ternary_operators_constructors/js/main.js
+++ Basic Javascript Projects/project6_ternary_operators_constructors/js/main.js
@@ -1,8 +1,8 @@
 function Ride_Function() {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+    var Height, Can_ride; //stating two variables
+    Height = document.getElementById("Height").value; //variable height placed in the input box in html
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough"; //2 outputs depending on true or false
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride."; //eventual output after user interacts
 }
 
 function age_validation() {
@@ -12,13 +12,13 @@ function age_validation() {
     document.getElementById("vote").innerHTML = can_vote;
 }
 
-function vehicle(make, model, year, color) {
+function vehicle(make, model, year, color) { //determining class and its variables
     this.vehicle_make = make;
     this.vehicle_model = model;
     this.vehicle_year = year;
     this.vehicle_color = color;
 }
-var Jack = new vehicle("Dodge", "Viper", 2020, "Red");
+var Jack = new vehicle("Dodge", "Viper", 2020, "Red"); //adding new labels that fit class variables
 var Emily = new vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new vehicle("Ford", "Pinto", 1971, "Mustard");
 function some_cars() {
@@ -35,12 +35,12 @@ function sport_drink(brand, flavor, price) {
 var vitarain = new sport_drink("kirkland", "dragonfruit", 2)
 function electrolyte() {
     document.getElementById("New_and_This").innerHTML = 
-    "The best sport drink available is the " +vitarain.flavor + " flavored drink by " +
+    "The best sport drink available is the " + vitarain.flavor + " flavored drink by " +
     vitarain.brand + " that costs $" + vitarain.price;
 }
 
 function counting() {
-    document.getElementById("Nested_Function").innerHTML = counting_more();
+    document.getElementById("Nested_Function").innerHTML = counting_more(); //calling nested function
         function counting_more() {
             let counter=0;
             function plus() {counter +=1;}
