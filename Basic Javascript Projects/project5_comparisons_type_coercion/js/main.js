a510a99a9596fe9e8cf89d746627ac1b7ca04a84
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-03 23:32:50 -0700

    commented on main.js

--- Basic Javascript Projects/project5_comparisons_type_coercion/js/main.js
+++ Basic Javascript Projects/project5_comparisons_type_coercion/js/main.js
@@ -7,16 +7,16 @@ function not_a_number() {
 }
 
 function true_func() {
-    document.getElementById("test_2").innerHTML=isNaN('Hello World');
+    document.getElementById("test_2").innerHTML=isNaN('Hello World'); //producing a "not a number"
 }
 
 function false_func() {
     document.getElementById("test_3").innerHTML=isNaN('82673');
 }
 
-document.write(4E422);
+document.write(4E422); //infinity
 
-document.write(-5E365);
+document.write(-5E365); //negative infinity
 
 document.write(10>3);
 
@@ -24,13 +24,13 @@ console.log(3+4);
 
 console.log(4>8);
 
-document.write(4==4);
+document.write(4==4); //comparing 2 values
 
 document.write(6==4);
 
 a=4;
 b=4;
-document.write(a===b);
+document.write(a===b); //comparing values and data type
 
 c=3;
 d="drink";
@@ -44,15 +44,15 @@ g="cake";
 h="pie";
 document.write(g===h);
 
-document.write(6>2 && 7>4);
+document.write(6>2 && 7>4); //determining logic between values, both have to be true
 
 document.write(6<2 && 7>4);
 
-document.write(4>5 || 8>3);
+document.write(4>5 || 8>3); //determinging logic between values, one has to be true
 
 document.write(9<2 || 8<4);
 
 function not_op() {
-    document.getElementById("not").innerHTML=!(6>9);
+    document.getElementById("not").innerHTML=!(6>9); //not operator
     document.getElementById("correct").innerHTML=!(6<9);
 }
