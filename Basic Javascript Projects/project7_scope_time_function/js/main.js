c0cb694fe9da1b0ab0931c8b05780795a96eb9c5
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-10-05 17:14:49 -0700

    commented on main.js

--- Basic Javascript Projects/project7_scope_time_function/js/main.js
+++ Basic Javascript Projects/project7_scope_time_function/js/main.js
@@ -1,15 +1,15 @@
-var x = 4;
+var x = 4; //global variable
 function multiply_1() {
     document.write(3*x + "<br>");
 }
 function multiply_2() {
     document.write(7*x + "<br>");
 }
-multiply_1();
+multiply_1(); //asking to run function
 multiply_2();
 
 function subtracting_1() {
-    var z = 11;
+    var z = 11; //local variable
     console.log(z - 9 + "<br>");
 }
 function subtracting_2() {
@@ -20,8 +20,8 @@ subtracting_1();
 subtracting_2();
 
 function time_func() {
-    if (new Date().getHours() <19) {
-        document.getElementById("scoping").innerHTML = "How was your day?";
+    if (new Date().getHours() <19) { //establishing parameters for response
+        document.getElementById("scoping").innerHTML = "How was your day?"; //setting response
     }
 }
 
@@ -33,11 +33,11 @@ function conditional() {
 
 function food_function() {
     answer = document.getElementById("yes_or_no").value;
-    if (answer === "yes") {
-        reward = "Good job! Keep yourself fed.";
+    if (answer === "yes") { //comparing input value to "yes"
+        reward = "Good job! Keep yourself fed."; //produced response IF value is yes
     }
     else {
-        reward = "You can have banana bread.";
+        reward = "You can have banana bread."; //produced response IF value is something other than yes
     }
     document.getElementById("food_response").innerHTML = reward;
 }
@@ -45,14 +45,14 @@ function food_function() {
 function Time_function() {
     var Time = new Date().getHours();
     var Reply;
-    if (Time < 12 == Time > 0) {
-        Reply = "It is morning time!";
+    if (Time < 12 == Time > 0) { //comparing current time to parameters
+        Reply = "It is morning time!"; //produced response IF time is in parameters
     }
-    else if (Time >= 12 == Time < 18) {
+    else if (Time >= 12 == Time < 18) { //comparing current time to parameters
         Reply = "It is afternoon time.";
     }
     else {
-        Reply = "It is evening time.";
+        Reply = "It is evening time."; //comparing current time to parameters
     }
     document.getElementById("Time_of_day").innerHTML = Reply;
 }
